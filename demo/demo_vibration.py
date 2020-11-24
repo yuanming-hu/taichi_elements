f0af5f69d1162098d60a784a2d2309dc78379de0
Yuanming Hu <yuanmhu@gmail.com>
2020-11-24 12:13:55 -0500

    text

--- demo/demo_vibration.py
+++ demo/demo_vibration.py
@@ -11,15 +11,19 @@ ti.init(arch=ti.cuda)  # Try to run on GPU
 
 res = 128
 
-gui = ti.GUI("Taichi Elements", res=res * 4, background_color=0x112F41)
+gui = ti.GUI("Taichi Elements", res=(res * 6, res * 3), background_color=0x112F41)
 
-E_scale = 2
-dt_scale = 3 / E_scale ** 0.5
+E_scale = 10
+dt_scale = 1 / E_scale ** 0.5
 mpm = MPMSolver(res=(res, res), E_scale=E_scale, dt_scale=dt_scale, unbounded=True)
 
-pattern = 1 - ti.imread('snowflake.png')[:, :, 1] * (1 / 255.0)
-pattern = cv2.resize(pattern, dsize=(256, 256), interpolation=cv2.INTER_CUBIC)
+for i in range(6):
+    mpm.add_cube(lower_corner=[0.15 + 0.3 * i, 0.1],
+                 cube_size=[0.05, 0.5],
+                 velocity=[0, 0],
+                 material=MPMSolver.material_elastic)
 
+'''
 mpm.add_surface_collider(point=(0, -0.6),
                          normal=(0.0, 1),
                          surface=mpm.surface_slip)
@@ -31,18 +35,19 @@ mpm.add_surface_collider(point=(1.4, 0),
 mpm.add_surface_collider(point=(-0.6, 0),
                          normal=(1, 0),
                          surface=mpm.surface_slip)
+'''
+
+omega_step = 6
 
 @ti.kernel
 def vibrate(t: ti.f32, dt: ti.f32):
     for I in ti.grouped(mpm.grid_m):
         p = I * mpm.dx
         if p[1] < 0.2:
-            mpm.grid_v[I] = ti.Vector([ti.sin(t * 10), 0.0])
+            omega = (p[0] // 0.3 + 1) * omega_step
+            mpm.grid_v[I] = ti.Vector([omega * 0.01 * ti.sin(t * omega), 0.0])
 
 mpm.grid_postprocess.append(vibrate)
-
-for i in range(1):
-    mpm.add_texture(-0.1 + 0.10 * i % 3, -0.1 + 0.6 * i, pattern)
 
 print(mpm.n_particles[None])
 
@@ -51,7 +56,10 @@ for frame in range(500):
     colors = np.array([0x068587, 0xED553B, 0xEEEEF0, 0xFFFF00],
                       dtype=np.uint32)
     particles = mpm.particle_info()
-    gui.circles(particles['position'] * 0.5 + 0.3,
+    gui.circles(particles['position'] / [[2, 1]],
                 radius=1.5,
                 color=colors[particles['material']])
+    gui.line(begin=(0, 0.2), end=(1, 0.2), radius=4, color=0xFFFFFF)
+    for i in range(6):
+        gui.text(f'omega={(i + 1) * omega_step:.2f}/sec', (0.15 * i + 0.05, 0.1))
     gui.show(f'outputs/{frame:06d}.png' if write_to_disk else None)
