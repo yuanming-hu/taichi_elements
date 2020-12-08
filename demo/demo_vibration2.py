2a5e3f52b9e8c758cf7d59829f73b787e70cb818
Yuanming Hu <yuanmhu@gmail.com>
2020-12-08 00:09:24 -0500

    .

--- demo/demo_vibration2.py
+++ demo/demo_vibration2.py
@@ -13,25 +13,40 @@ res = 128
 
 gui = ti.GUI("Taichi Elements", res=(res * 15, res * 6), background_color=0x112F41)
 
-E_scale = 50
+E_scale = 200
 dt_scale = 1 / E_scale ** 0.5
 mpm = MPMSolver(res=(res, res), E_scale=E_scale, dt_scale=dt_scale, unbounded=True)
 
 space = 0.7
 
 for i in range(3):
-    for j in range(2):
-        offset_x = space * i + 0.2 * j
-        mpm.add_cube(lower_corner=[0.15 + offset_x, 0.35],
-                     cube_size=[0.05, 0.5],
-                     velocity=[0, 0],
-                     material=MPMSolver.material_elastic)
+    offset_x = space * i - 0.1
+    mpm.add_cube(lower_corner=[0.15 + offset_x, 0.35],
+                 cube_size=[0.05, 0.5],
+                 velocity=[0, 0],
+                 material=MPMSolver.material_elastic)
+    
+    mpm.add_cube(lower_corner=[0.15 + offset_x + 0.2, 0.25],
+                 cube_size=[0.1, 0.6],
+                 velocity=[0, 0],
+                 material=MPMSolver.material_elastic)
+    
+    mpm.add_cube(lower_corner=[0.18 + offset_x + 0.1, 0.25],
+                 cube_size=[0.07, 0.05],
+                 velocity=[0, 0],
+                 material=MPMSolver.material_elastic)
 
-        mpm.add_ellipsoid(center=[0.175 + offset_x, 0.3], radius=0.07, material=MPMSolver.material_elastic)
+    mpm.add_ellipsoid(center=[0.175 + offset_x, 0.3], radius=0.07, material=MPMSolver.material_elastic)
+    
+    
+    # mpm.add_cube(lower_corner=[0.275 + offset_x, 0.1],
+    #              cube_size=[0.01, 0.4],
+    #              velocity=[0, 0],
+    #              material=MPMSolver.material_elastic)
         
-    mpm.add_ellipsoid(center=[0.275 + space * i, 0.4], radius=0.05, material=MPMSolver.material_elastic)
+    mpm.add_ellipsoid(center=[0.275 + offset_x, 0.5], radius=0.05, material=MPMSolver.material_elastic)
 
-omega_step = 6
+omega_step = 27
 
 @ti.kernel
 def vibrate(t: ti.f32, dt: ti.f32):
@@ -39,7 +54,7 @@ def vibrate(t: ti.f32, dt: ti.f32):
         p = I * mpm.dx
         if p[1] > 0.8:
             omega = (p[0] // space + 1) * omega_step
-            mpm.grid_v[I] = ti.Vector([omega * 0.02 * ti.cos(t * omega), 0.0])
+            mpm.grid_v[I] = ti.Vector([omega * 0.01 * ti.sin(t * omega), 0.0])
 
 mpm.grid_postprocess.append(vibrate)
 
