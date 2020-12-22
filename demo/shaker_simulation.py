3eea615286c2fd9686273f83e78d727bedeef15b
Yuanming Hu <yuanmhu@gmail.com>
2020-12-22 12:03:44 -0500

    shader sim

--- demo/shaker_simulation.py
+++ demo/shaker_simulation.py
@@ -7,12 +7,17 @@ from engine.mpm_solver import MPMSolver
 
 
 class ShaderSimulation:
-    def __init__(self):
+    def __init__(self,
+                 res=256,
+                 gui_scale=3,
+                 frame_dt=1 / 160,
+                 shaker_omega=200,
+                 shaker_magnitude=0.005):
         ti.init(arch=ti.cuda)  # Try to run on GPU
         self.write_to_disk = True
 
-        self.res = 256
-        self.gui_scale = 3
+        self.res = res
+        self.gui_scale = gui_scale
 
         self.gui = ti.GUI("Taichi Elements",
                           res=(self.res * self.gui_scale,
@@ -27,27 +32,24 @@ class ShaderSimulation:
                         unbounded=True)
         mpm.set_gravity([0, -1])
 
-        pattern = 1 - ti.imread('snowflake.png')[:, :, 1] * (1 / 255.0)
-        dsize = 128
-        pattern = cv2.resize(pattern,
-                             dsize=(dsize, dsize),
-                             interpolation=cv2.INTER_CUBIC)
-
-        space = 0.7
-
-        omega = 200
-        mag = 0.005
+        self.omega = shaker_omega
+        self.mag = shaker_magnitude
         initial_offset = 0.1
+        self.initial_offset = initial_offset
         shaker_width = 0.3
-        ground_y = 0.2
+        self.shaker_width = shaker_width
+        ground_y = 0.1
+        self.ground_y = ground_y
         block_height = 0.1
+        self.block_height = block_height
 
         for i in range(3):
             offset_y = 0.33 * i + block_height * 1.2
-            mpm.add_cube(lower_corner=[initial_offset, ground_y + offset_y],
-                         cube_size=[shaker_width, block_height],
-                         velocity=[0, 0],
-                         material=MPMSolver.material_elastic)
+            mpm.add_cube(
+                lower_corner=[self.initial_offset, ground_y + offset_y],
+                cube_size=[shaker_width, block_height],
+                velocity=[0, 0],
+                material=MPMSolver.material_elastic)
 
             num_tooth = 4
             tooth_width = shaker_width / num_tooth / 2
@@ -73,12 +75,13 @@ class ShaderSimulation:
         def vibrate(t: ti.f32, dt: ti.f32):
             for I in ti.grouped(mpm.grid_m):
                 p = I * mpm.dx
-                if p[1] < 0.2:
+                if p[1] < ground_y:
                     mpm.grid_v[I].y = 0
-                slab_offset = ti.sin(t * omega) * mag + initial_offset
+                slab_offset = ti.sin(
+                    t * self.omega) * self.mag + initial_offset
                 pos_left = slab_offset
                 pos_right = pos_left + shaker_width
-                shaker_v = omega * ti.cos(t * omega) * mag
+                shaker_v = self.omega * ti.cos(t * self.omega) * self.mag
                 if p[0] < pos_left:
                     mpm.grid_v[I][0] = shaker_v
                 if p[0] > pos_right:
@@ -86,32 +89,42 @@ class ShaderSimulation:
 
         mpm.grid_postprocess.append(vibrate)
 
-        print(mpm.n_particles[None])
+        print('num_particles:', mpm.n_particles[None])
 
-        frame_dt = 1 / 160
+        self.frame = 0
+        self.frame_dt = frame_dt
+        self.mpm = mpm
 
-        for frame in range(3000):
-            mpm.step(frame_dt)
-            colors = np.array([0x068587, 0xED553B, 0xEEEEF0, 0xFFFF00],
-                              dtype=np.uint32)
-            particles = mpm.particle_info()
-            gui = self.gui
-            gui.circles(particles['position'] / [[1, 1]],
-                        radius=2,
-                        color=colors[particles['material']])
-            gui.line(begin=(0, 0.2), end=(1, 0.2), radius=3, color=0xFFFFFF)
-            offset = math.sin(
-                (frame + 0.5) * frame_dt * omega) * mag + initial_offset
-            gui.line(begin=(offset, 0.2),
-                     end=(offset, 1.0),
-                     radius=3,
-                     color=0xFFFFFF)
-            gui.line(begin=(offset + shaker_width, ground_y),
-                     end=(offset + shaker_width, 1.0),
-                     radius=3,
-                     color=0xFFFFFF)
-            gui.show(
-                f'outputs5/{frame:06d}.png' if self.write_to_disk else None)
+    def advance(self):
+        self.mpm.step(self.frame_dt)
+        colors = np.array([0x068587, 0xED553B, 0xEEEEF0, 0xFFFF00],
+                          dtype=np.uint32)
+        particles = self.mpm.particle_info()
+        gui = self.gui
+        gui.circles(particles['position'] / [[1, 1]],
+                    radius=2,
+                    color=colors[particles['material']])
+        gui.line(begin=(0, self.ground_y),
+                 end=(1, self.ground_y),
+                 radius=3,
+                 color=0xFFFFFF)
+        offset = math.sin((self.frame + 0.5) * self.frame_dt *
+                          self.omega) * self.mag + self.initial_offset
+        gui.line(begin=(offset, self.ground_y),
+                 end=(offset, 1.0),
+                 radius=3,
+                 color=0xFFFFFF)
+        gui.line(begin=(offset + self.shaker_width, self.ground_y),
+                 end=(offset + self.shaker_width, 1.0),
+                 radius=3,
+                 color=0xFFFFFF)
+        gui.show(
+            f'outputs5/{self.frame:06d}.png' if self.write_to_disk else None)
+
+        self.frame += 1
 
 
-shaker = ShaderSimulation()
+shaker = ShaderSimulation(frame_dt=1 / 160)
+
+for i in range(100):
+    shaker.advance()
