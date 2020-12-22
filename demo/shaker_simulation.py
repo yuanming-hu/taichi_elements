f0df9168ce479356d19687a8dd3cc238532b09c9
Yuanming Hu <yuanmhu@gmail.com>
2020-12-22 14:35:50 -0500

    pusher

--- demo/shaker_simulation.py
+++ demo/shaker_simulation.py
@@ -11,14 +11,16 @@ class ShaderSimulation:
                  gui_scale=3,
                  frame_dt=1 / 160,
                  shaker_omega=200,
-                 shaker_magnitude=0.005, gravity=[0, -2]):
+                 shaker_magnitude=0.005,
+                 shaker_width=0.7,
+                 gravity=[0, -2]):
         ti.init(arch=ti.cuda)  # Try to run on GPU
         self.write_to_disk = True
 
         self.res = res
         self.gui_scale = gui_scale
 
-        self.gui = ti.GUI("Taichi Elements",
+        self.gui = ti.GUI("Shaker Simulation",
                           res=(self.res * self.gui_scale,
                                self.res * self.gui_scale),
                           background_color=0x112F41)
@@ -35,12 +37,13 @@ class ShaderSimulation:
         self.mag = shaker_magnitude
         initial_offset = 0.1
         self.initial_offset = initial_offset
-        shaker_width = 0.3
         self.shaker_width = shaker_width
         ground_y = 0.1
         self.ground_y = ground_y
         block_height = 0.1
         self.block_height = block_height
+        self.pusher_initial_height = 2.0
+        self.pusher_final_height = 0.4
 
         @ti.kernel
         def vibrate(t: ti.f32, dt: ti.f32):
@@ -58,43 +61,58 @@ class ShaderSimulation:
                 if p[0] > pos_right and mpm.grid_v[I][0] > shaker_v:
                     mpm.grid_v[I][0] = shaker_v
 
+                pusher_v = self.pusher_final_height - self.pusher_initial_height
+
+                if p[0] > pos_right and mpm.grid_v[I][0] > shaker_v:
+                    mpm.grid_v[I][0] = shaker_v
+
+                pusher_current_height = pusher_v * t + self.pusher_initial_height
+
+                if pusher_current_height < self.pusher_final_height:
+                    pusher_v = 0.0
+                    pusher_current_height = self.pusher_final_height
+
+                if p[1] > pusher_current_height and mpm.grid_v[I][1] > pusher_v:
+                    mpm.grid_v[I][1] = pusher_v
+
         mpm.grid_postprocess.append(vibrate)
 
         self.frame = 0
         self.frame_dt = frame_dt
         self.mpm = mpm
 
-    def create_snowflakes(
-        self,
-        core_radius=0.03,
-        num_rods=7,
-        rod_length=0.02,
-        rod_thickness_deg=15,
-        end_radius=0.01
-    ):
+    def create_snowflakes(self,
+                          num_snowflakes=10,
+                          core_radius=0.03,
+                          num_rods=7,
+                          rod_length=0.02,
+                          rod_thickness_deg=15,
+                          end_radius=0.01):
         mpm = self.mpm
-        for i in range(10):
+        for i in range(num_snowflakes):
 
             @ti.func
             def sdf(x):
                 phi = ti.atan2(x[1], x[0])
                 r = x.norm()
-                
+
                 rad = 2 * math.pi / num_rods
                 phi = (phi + rad * 0.5) % rad - rad * 0.5
 
                 x = ti.Vector([r * ti.cos(phi), r * ti.sin(phi)])
                 rod_radius = core_radius + rod_length
-                
+
                 rod_thickness_rad = math.radians(rod_thickness_deg)
-                return (r < rod_radius and -0.5 * rod_thickness_rad < phi < 0.5 * rod_thickness_rad
-                        ) or r < core_radius or (x - ti.Vector([rod_radius, 0])).norm() < end_radius
+                return (r < rod_radius and -0.5 * rod_thickness_rad < phi <
+                        0.5 * rod_thickness_rad) or r < core_radius or (
+                            x - ti.Vector([rod_radius, 0])).norm() < end_radius
 
             # mpm.add_texture_2d(initial_offset + 0.25 * (i % 3), 0.2 + 0.15 * i,
             #                    pattern)
             mpm.add_particles_inside_sdf(
-                self.initial_offset + self.shaker_width * 0.17 + self.shaker_width * 0.3 * (i % 3), 0.2 + 0.10 * i,
-                sdf, self.res)
+                self.initial_offset + self.shaker_width * 0.17 +
+                self.shaker_width * 0.3 * (i % 3), 0.2 + 0.15 * i, sdf,
+                self.res)
 
     def create_bricks(self):
         mpm = self.mpm
@@ -146,10 +164,19 @@ class ShaderSimulation:
                  end=(offset, 1.0),
                  radius=3,
                  color=0xFFFFFF)
+
+        pusher_y = self.pusher_initial_height + self.mpm.t * (
+            self.pusher_final_height - self.pusher_initial_height)
+        pusher_y = max(pusher_y, self.pusher_final_height)
         gui.line(begin=(offset + self.shaker_width, self.ground_y),
                  end=(offset + self.shaker_width, 1.0),
                  radius=3,
                  color=0xFFFFFF)
+
+        gui.line(begin=(offset, pusher_y),
+                 end=(offset + self.shaker_width, pusher_y),
+                 radius=3,
+                 color=0xFF2233)
         gui.show(
             f'outputs5/{self.frame:06d}.png' if self.write_to_disk else None)
 
@@ -162,9 +189,14 @@ class ShaderSimulation:
             self.advance()
 
 
-sim = ShaderSimulation(frame_dt=1 / 160)
+sim = ShaderSimulation(frame_dt=1 / 160, shaker_width=0.7)
 
 # sim.create_bricks()
-sim.create_snowflakes()
+sim.create_snowflakes(num_snowflakes=10,
+                      core_radius=0.04,
+                      num_rods=7,
+                      rod_length=0.03,
+                      rod_thickness_deg=15,
+                      end_radius=0.015)
 
 sim.run(1000)
